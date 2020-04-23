8ee9a9c0075beee2ec525be825a4e6b8413b0316
Freddie Levenson <freddielev@gmail.com>
2020-04-22 18:38:05 -0600

    Add boilerplate code to each page

---
+++ pages/contact/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Contact info</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+          <p>Contact not setup</p>
+      </main>
+
+      <style jsx>{`
+      `}</style>
+    </div>
+  )
+}
---
+++ pages/portfolio/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Portfolio</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+          <p>Portfolio page not set up</p>
+      </main>
+
+      <style jsx>{`
+      `}</style>
+    </div>
+  )
+}
