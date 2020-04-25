33fe5a85cc3e80fc15bb22cfb418b67788112438
Freddie Levenson <freddielev@gmail.com>
2020-04-25 16:22:20 -0600

    Change skyline picture and add homepage blurb

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,11 @@ export default function Home() {
       </Head>
       <MainLayout />
       <main>
-        <div className='main-blurb'>
-          <div></div>
+        <div className='main-section'>
+          <div className='blurb'>
+            <p>Welcome to my portfolio site. I am a developer residing in Denver, CO specializing in Front End website development. Coming from a background in Fine Art I have found software engineering to be a fulfilling outlet for my creativity, giving me an endless canvas to bring my ideas to life. My background gives me both a keen eye for design and an ability to think outside the box when searching for solutions.</p>
+            <p>I enjoy using my skills to help others and look forward to new opportunities to collaborate. I'm always looking for a project to sink my teeth into so please reach out on my contact page if you have a project I can help with. Check out the portfolio tab to see some examples of past projects I've worked on. Thanks for visiting!</p>
+          </div>
           <img className='denver' src='denver-skyline-2.jpeg' />
         </div>
         <div className='icon-holder'>
@@ -50,7 +53,7 @@ export default function Home() {
         justify-content: center;
       }
 
-      .main-blurb {
+      .main-section {
         width: 70vw;
         height: 89.5vh;
         display: flex;
@@ -62,6 +65,16 @@ export default function Home() {
         font-size: 1.3rem;
         flex-direction: column;
         justify-content: space-between;
+      }
+
+      .blurb {
+        height: 60%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-around;
+        padding: 40px;
+        line-height: 1.6rem;
       }
 
       .denver {
